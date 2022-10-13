1a24f617c429a2c3f92e31f671ff7a665fd3b34c
thomasNewton <tnewtonom@gmail.com>
2022-10-12 17:30:18 -0700

    modified:   css/test2Css.css 	modified:   script/test2Script.js 	modified:   test2.html

--- script/test2Script.js
+++ script/test2Script.js
@@ -11,21 +11,21 @@ function test_text(){
     document.getElementById("console_text_area").innerHTML+="\nnew text";
 }
 function set_bom_txt() {
-    document.getElementById("console_text_area").innerHTML="";
-    document.getElementById("console_text_area").innerHTML+="Screen width:" + screen.width;
-    document.getElementById("console_text_area").innerHTML+="\tScreen Height:" + screen.height;
-    document.getElementById("console_text_area").innerHTML+="\tAvailable Screen Width:" + screen.availWidth;
-    document.getElementById("console_text_area").innerHTML+="\tAvailable Screen Height:" + screen.availHeight;
-    document.getElementById("console_text_area").innerHTML+="\nScreen Color Depth:" + screen.colorDepth;
-    document.getElementById("console_text_area").innerHTML+="\tScreen Pixel Depth:" + screen.pixelDepth;
-    document.getElementById("console_text_area").innerHTML+="\tPage location:" + window.location.href;
-    document.getElementById("console_text_area").innerHTML+="\nPage hostname:" + window.location.hostname;
-    document.getElementById("console_text_area").innerHTML+="\tPage path:" + window.location.pathname;
-    document.getElementById("console_text_area").innerHTML+="\tPage protocol:" + window.location.protocol;
-    document.getElementById("console_text_area").innerHTML+="\tPort number:" + window.location.port;
-    document.getElementById("console_text_area").innerHTML+="\ncookiesEnabled:" + navigator.cookieEnabled;
-    document.getElementById("console_text_area").innerHTML+="\tnavigator.appName:" +navigator.appName;
-    document.getElementById("console_text_area").innerHTML+="\tnavigator platform:"+ navigator.platform;
-    document.getElementById("console_text_area").innerHTML+="\tnavigator online"+navigator.onLine;
+    document.getElementById("console_text_area").innerHTML="";setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="Screen width:" + screen.width;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tScreen Height:" + screen.height;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tAvailable Screen Width:" + screen.availWidth;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tAvailable Screen Height:" + screen.availHeight;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\nScreen Color Depth:" + screen.colorDepth;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tScreen Pixel Depth:" + screen.pixelDepth;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tPage location:" + window.location.href;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\nPage hostname:" + window.location.hostname;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tPage path:" + window.location.pathname;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tPage protocol:" + window.location.protocol;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tPort number:" + window.location.port;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\ncookiesEnabled:" + navigator.cookieEnabled;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tnavigator.appName:" +navigator.appName;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tnavigator platform:"+ navigator.platform;setTimeout(250);
+    document.getElementById("console_text_area").innerHTML+="\tnavigator online"+navigator.onLine;setTimeout(250);
    
-}
+    ---------------
